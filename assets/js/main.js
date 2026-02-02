ebedde69448b759099bb3920613a2c95a9755ad0
raphaellebas <raphael.lebas@outlook.fr>
2026-02-02 14:41:25 +0100

    Refacto: JSMedia Querie -> CSS Media Querie

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,6 @@
 //Import our function
 import { initHeader } from "./includes/header.js";
-import { mediaHeader } from "./mediaQuerie/mediaHeader.js"
-import { mediaFooter } from "./mediaQuerie/mediaFooter.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     initHeader();
-    mediaFooter();
-    mediaHeader();
 });
